ca3a109a12ebd55e2379748a4752ff4c05b75afc
sr1983-it-gl <sr1983.it.gl@gmail.com>
2022-04-10 16:21:30 +0530

    Files

--- src/com/gl/datastructures/common/DataStructureUtils.java
+++ src/com/gl/datastructures/common/DataStructureUtils.java
@@ -78,7 +78,23 @@ public class DataStructureUtils {
 		}
 		System.out.println(" }");
 	}
-	
+
+	public static String printAndReturn(int[] array) {
+		
+		StringBuilder result = new StringBuilder();
+		result.append("{ ");
+		for (int index = 0; index < array.length; index ++) {	
+
+			result.append(array[index]);
+
+			if (index != (array.length - 1)) {
+				result.append(", ");
+			}
+		}
+		result.append(" }");
+		return result.toString();
+	}
+
 	private static int generateInitialRandomNumber() {
 		
 		double random = (Math.random() * 10) + 1;
